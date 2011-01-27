c4ca8914ef8dcff94c841b11ac14d76b7b7b1dc9
Tero Piirainen <tipiirai@gmail.com>
2011-01-27 12:46:30 +0000

    redis story

--- media/js/site.js
+++ media/js/site.js
@@ -20,7 +20,7 @@
 				 diff < 120 && (future ? "a minute" : "1 minute ago") ||
 				 diff < 3600 && str("_ minutes", "_ minutes ago", Math.floor( diff / 60 )) ||
 				 diff < 7200 && (future ? "one hour" : "1 hour ago") ||
-				 diff < 86400 && str("_ hours", "_ hours ago", Math.floor(diff / 3600))) ||
+				 diff < 86400 && str("_ h", "_ hours ago", Math.floor(diff / 3600))) ||
 				 day_diff == 1 && (future ? "Tomorrow" : "Yesterday") ||
 				 day_diff < 7 && str("_ days", "_ days ago", day_diff) ||
 				 day_diff == 7 && (future ? "a week" : "week ago") ||
